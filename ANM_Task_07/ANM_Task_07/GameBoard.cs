aed918097e6fffda2368a72cebf94d5f4baa3851
205-22 <33nasrudin@gmail.com>
2025-03-13 15:26:37 +0300

    end

--- ANM_Task_07/ANM_Task_07/GameBoard.cs
+++ ANM_Task_07/ANM_Task_07/GameBoard.cs
@@ -19,14 +19,24 @@ namespace ANM_Task_07
 				((point.X == 5 || point.X == 6) && (point.Y == 0 || point.Y == 1));
 		}
 
-		public static bool IsValidMove(Point from, Point to)
-		{
-			// Проверка на допустимость хода
-			if (Math.Abs(from.X - to.X) + Math.Abs(from.Y - to.Y) == 1)
-			{
-				return true;
-			}
-			return false;
-		}
-	}
+        public static bool IsValidMove(Point from, Point to, ICollection<Point> chickens = null, ICollection<Point> foxes = null)
+        {
+            // Куры не могут двигаться вниз
+            if (to.Y > from.Y) return false;
+
+            // Проверка на допустимость хода
+            if (!(Math.Abs(from.X - to.X) + Math.Abs(from.Y - to.Y) == 1))
+            {
+                return false;
+            }
+
+            // Проверка зянята ли клетка другими объектами
+            if ((chickens != null && foxes != null) && (chickens.Contains(to) || foxes.Contains(to)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
